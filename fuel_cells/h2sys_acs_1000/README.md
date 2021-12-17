29d62e0320ffb9755124438a1d0e2abee82d4d99
Tatyana <tyugaj@enapter.com>
2021-12-17 18:31:05 +0300

    Finalize H2Sys Aircell

--- fuel_cells/h2sys_acs_1000/README.md
+++ fuel_cells/h2sys_acs_1000/README.md
@@ -1,12 +1,13 @@
 # H2Sys AIRCELL ACS 1000
 
-This _Enapter Device Blueprint_ integrates **H2Sys AIRCELL ACS 1000** - hydrogen air cooled fuel cell system with [CAN bus](https://developers.enapter.com/docs/reference/ucm/can). It also supports Modbus TCP.
+This [Enapter Device Blueprint](https://github.com/Enapter/marketplace#blue_book-enapter-device-blueprints) integrates **H2Sys AIRCELL ACS 1000** - hydrogen air cooled fuel cell system with [CAN bus](https://developers.enapter.com/docs/reference/ucm/can). It also supports Modbus TCP.
 
-Use [Enapter ENP-CAN](https://handbook.enapter.com/modules/ENP-CAN/ENP-CAN.html) module for physical connection. See [connection instructions](https://handbook.enapter.com/modules/ENP-CAN/ENP-CAN.html#connection-examples) in the module manual.
+## Connect to Enapter
 
-## CAN bus Communication Interface Parameters
-
-- Baud rate: `500` kbps.
+- Sign up to Enapter Cloud using [Web](https://cloud.enapter.com/) or mobile app ([iOS](https://apps.apple.com/app/id1388329910), [Android](https://play.google.com/store/apps/details?id=com.enapter&hl=en)).
+- Use [Enapter ENP-CAN](https://handbook.enapter.com/modules/ENP-CAN/ENP-CAN.html) module for physical connection. See [connection instructions](https://handbook.enapter.com/modules/ENP-CAN/ENP-CAN.html#connection-examples) in the module manual.
+- [Add ENP-CAN to your site](https://handbook.enapter.com/software/mobile/android_mobile_app.html#adding-sites-and-devices) using the mobile app.
+- [Upload](https://developers.enapter.com/docs/tutorial/uploading-blueprint/) this blueprint to ENP-CAN.
 
 ## References
 
--- fuel_cells/h2sys_acs_1000/firmware.lua
+++ fuel_cells/h2sys_acs_1000/firmware.lua
@@ -1,126 +1,73 @@
+-- CAN bus communication interface paramter
+BAUD_RATE = 500 -- kbps
+
+received_alerts_packet = false
 telemetry = {}
+alerts = {}
 
 function main()
-  local result = can.init(500, can_handler)
+  local result = can.init(BAUD_RATE, can_handler)
   if result ~= 0 then
     enapter.log("CAN failed: "..result.." "..can.err_to_str(result), "error", true)
+    scheduler.add(5000, function()
+      enapter.send_telemetry({ status = 'error', alerts = {'can_init_failed'} })
+    end)
+  else
+    scheduler.add(30000, send_properties)
+    scheduler.add(1000, send_telemetry)
   end
-
-  scheduler.add(30000, properties)
-  scheduler.add(1000, metrics)
 end
 
-function properties()
+function send_properties()
   enapter.send_properties({ vendor = "H2SYS", model = "ACS 1000" })
 end
 
-function metrics()
-  if next(telemetry) ~= nil then
+-- Holds the number of `send_telemetry` that had an empty `telemetry`
+local missed_telemetry_count = 0
+
+function send_telemetry()
+  if telemetry[1] ~= nil then
+    if received_alerts_packet then
+      telemetry.alerts = alerts
+      alerts = {}
+      received_alerts_packet = false
+    end
+
     enapter.send_telemetry(telemetry)
+    -- Cleanup telemetry and let it be refilled by `can_handler`
+    telemetry = {}
+    missed_telemetry_count = 0
   else
-    enapter.log("No data", error, true)
+    missed_telemetry_count = missed_telemetry_count + 1
+    if missed_telemetry_count > 5 then
+      enapter.send_telemetry({
+        status = 'read_error',
+        alerts = {'communication_failed'}
+      })
+    end
   end
 end
 
 function can_handler(msg_id, data)
   if msg_id == 0x666 then
-    local byte0, byte1, byte3 = string.unpack("I1I1I1", data)
-    local warning = "ok"
-    local errors = {}
-
-    if byte0 & 1 ~=0 then
-      table.insert(errors, "low_cell_voltage")
-    end
-    if byte0 & 2 ~=0 then
-      table.insert(errors, "fuel_cell_high_temperature")
-    end
-    if byte0 & 4 ~= 0 then
-      table.insert(errors, "fuel_cell_low_voltage")
-    end
-    if byte0 & 8 ~= 0 then
-      table.insert(errors, "low_h2_pressure")
-    end
-    if byte0 & 16 ~= 0 then
-      table.insert(errors, "auxilary_voltage")
-    end
-    if byte0 & 32 ~= 0 then
-      warning = "fcvm_board"
-    end
-    if byte0 & 64 ~= 0 then
-      warning = "idle_state"
-    end
-    if byte0 & 128 ~= 0 then
-      table.insert(errors, "start_phase_error")
-    end
-
-    if byte1 & 1 ~=0 then
-      table.insert(errors, "fccc_board")
-    end
-    if byte1 & 2 ~=0 then
-      table.insert(errors, "can_fail")
-    end
-    if byte1 & 4 ~= 0 then
-      table.insert(errors, "fuel_cell_current")
-    end
-    if byte1 & 16 ~= 0 then
-      table.insert(errors, "fan_error")
-    end
-    if byte1 & 32 ~= 0 then
-      table.insert(errors, "h2_leakage")
-    end
-    if byte1 & 64 ~= 0 then
-      table.insert(errors, "low_internal_pressure")
-    end
-    if byte1 & 128 ~= 0 then
-      table.insert(errors, "high_internal_pressure")
-    end
-
-    if byte3 & 1 ~= 0 then
-      table.insert(errors, "fuel_cell_temperature_variation")
-    end
-    telemetry["warning"] = warning
-    telemetry["alerts"] = errors
+    get_faults(data)
+    received_alerts_packet = true
   end
 
   if msg_id == 0x090 then
-    local voltage, current = string.unpack(">I2>I2", data)
-    telemetry["voltage"] = 0.0216 * voltage
-    telemetry["current"] = (current - 2048) * 0.06105
-  end
-
-  if msg_id == 0x091 then
-    local temperature, _, _, _, internal_h2_pressure, state = string.unpack("I1I1I1I1I1I1", data)
-    telemetry["temperature"] = to7bits(temperature)
-    telemetry["internal_h2_pressure"] = to7bits(internal_h2_pressure) / 10.0
-    if state == 0 then
-      telemetry["state"] = "auto_check"
-    elseif state == 1 then
-      telemetry["state"] = "h2_inlet_pressure"
-    elseif state == 10 then
-      telemetry["state"] = "waiting"
-    elseif state >= 11 and state <= 14  then
-      telemetry["state"] = "start-up"
-    elseif state == 15  then
-      telemetry["state"] = "idle"
-    elseif state == 17  then
-      telemetry["state"] = "operation"
-    elseif state == 21  then
-      telemetry["state"] = "switch_off"
-    elseif state == 22  then
-      telemetry["state"] = "locked_out"
+    local ok, err = pcall(function()
+      local voltage, current = string.unpack(">I2>I2", data)
+      telemetry["voltage"] = 0.0216 * voltage
+      telemetry["current"] = (current - 2048) * 0.06105
+    end)
+    if not ok then
+      table.insert(telemetry["alerts"], "communication_failed")
+      enapter.log("Msg_id 0x090 data failed: "..err, "error")
     end
   end
 
-end
-
-function to7bits(data)
-  local value = data & 0x7F
-  local sign = data >> 7
-
-  if sign == 1 then
-    return value * (-1)
-  else
-    return value
+  if msg_id == 0x091 then
+    get_system_info(data)
   end
 end
 
@@ -145,5 +92,111 @@ enapter.register_command_handler("stop", function(ctx)
     ctx.error("CAN failed")
   end
 end)
+
+function get_faults(data)
+  local ok, err = pcall(function()
+    local byte0, byte1, byte3 = string.unpack("I1I1I1", data)
+    local warning = "ok"
+    local alerts = {}
+
+    if byte0 & 1 ~=0 then
+      table.insert(alerts, "low_cell_voltage")
+    end
+    if byte0 & 2 ~=0 then
+      table.insert(alerts, "fuel_cell_high_temperature")
+    end
+    if byte0 & 4 ~= 0 then
+      table.insert(alerts, "fuel_cell_low_voltage")
+    end
+    if byte0 & 8 ~= 0 then
+      table.insert(alerts, "low_h2_pressure")
+    end
+    if byte0 & 16 ~= 0 then
+      table.insert(alerts, "auxilary_voltage")
+    end
+    if byte0 & 32 ~= 0 then
+      warning = "fcvm_board"
+    end
+    if byte0 & 64 ~= 0 then
+      warning = "idle_state"
+    end
+    if byte0 & 128 ~= 0 then
+      table.insert(alerts, "start_phase_error")
+    end
+
+    if byte1 & 1 ~=0 then
+      table.insert(alerts, "fccc_board")
+    end
+    if byte1 & 2 ~=0 then
+      table.insert(alerts, "can_fail")
+    end
+    if byte1 & 4 ~= 0 then
+      table.insert(alerts, "fuel_cell_current")
+    end
+    if byte1 & 16 ~= 0 then
+      table.insert(alerts, "fan_error")
+    end
+    if byte1 & 32 ~= 0 then
+      table.insert(alerts, "h2_leakage")
+    end
+    if byte1 & 64 ~= 0 then
+      table.insert(alerts, "low_internal_pressure")
+    end
+    if byte1 & 128 ~= 0 then
+      table.insert(alerts, "high_internal_pressure")
+    end
+
+    if byte3 & 1 ~= 0 then
+      table.insert(alerts, "fuel_cell_temperature_variation")
+    end
+
+    telemetry["warning"] = warning
+    telemetry["alerts"] = alerts
+  end)
+  if not ok then
+    telemetry["alerts"] = {"communication_failed"}
+    enapter.log("Msg_id 0x666 data failed: "..err, "error")
+  end
+end
+
+function get_system_info(data)
+  local ok, err = pcall(function()
+    local temperature, _, _, _, internal_h2_pressure, state = string.unpack("I1I1I1I1I1I1", data)
+    telemetry["temperature"] = to7bits(temperature)
+    telemetry["internal_h2_pressure"] = to7bits(internal_h2_pressure) / 10.0
+    if state == 0 then
+      telemetry["status"] = "auto_check"
+    elseif state == 1 then
+      telemetry["status"] = "h2_inlet_pressure"
+    elseif state == 10 then
+      telemetry["status"] = "waiting"
+    elseif state >= 11 and state <= 14  then
+      telemetry["status"] = "start-up"
+    elseif state == 15  then
+      telemetry["status"] = "idle"
+    elseif state == 17  then
+      telemetry["status"] = "operation"
+    elseif state == 21  then
+      telemetry["status"] = "switch_off"
+    elseif state == 22  then
+      telemetry["status"] = "locked_out"
+    end
+  end)
+  if not ok then
+    table.insert(telemetry["alerts"], "communication_failed")
+    enapter.log("Msg_id 0x091 data failed: "..err, "error")
+  end
+end
+
+function to7bits(data)
+  local value = data & 0x7F
+  local sign = data >> 7
+
+  if sign == 1 then
+    return value * (-1)
+  else
+    return value
+  end
+end
 
 main()
