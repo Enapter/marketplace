22598b86f372143545e458b131b5a62c0250a084
Rina-an <86365235+Rina-an@users.noreply.github.com>
2022-05-06 16:50:03 +0300

    Indentation update

--- firmware.lua
+++ firmware.lua
@@ -26,30 +26,30 @@ function send_telemetry ()
 
   for id = 1, 7 do
     local di_is_closed, err = di7.is_closed(id)
-     if di_is_closed == nil then
-       status = "error"
-       enapter.log("Reading closed di"..id.." failed: "..di7.err_to_st(err))
-	     table.insert(alerts, "DI"..id.."_read_closed_failed")
-     else
-       telemetry["di"..id.."_closed"] = di_is_closed
-     end
-     local di_is_opened, err = di7.is_opened(id)
-     if di_is_opened == nil then
-       status = "error"
-       enapter.log("Reading open di"..id.." failed: "..di7.err_to_st(err))
-	     table.insert(alerts, "DI"..id.."_read_closed_failed")
-     else
-       telemetry["di"..id.."_opened"] = di_is_opened
-     end
-     local counter, reset_time, err = di7.read_counter(id)
-     if counter == nil then
-	     status = "error"
-       enapter.log("Reading counter di"..id.." failed: "..di7.err_to_st(err))
-	     table.insert(alerts, "DI"..id.."_read_closed_failed")
-     else
-       telemetry["di"..id.."_counter"] = counter
-       telemetry["di"..id.."_reset_time"] = reset_time
-     end
+      if di_is_closed == nil then
+        status = "error"
+        enapter.log("Reading closed di"..id.." failed: "..di7.err_to_st(err))
+	      table.insert(alerts, "DI"..id.."_read_closed_failed")
+      else
+        telemetry["di"..id.."_closed"] = di_is_closed
+      end
+    local di_is_opened, err = di7.is_opened(id)
+      if di_is_opened == nil then
+        status = "error"
+        enapter.log("Reading open di"..id.." failed: "..di7.err_to_st(err))
+	      table.insert(alerts, "DI"..id.."_read_closed_failed")
+      else
+        telemetry["di"..id.."_opened"] = di_is_opened
+      end
+    local counter, reset_time, err = di7.read_counter(id)
+      if counter == nil then
+	      status = "error"
+        enapter.log("Reading counter di"..id.." failed: "..di7.err_to_st(err))
+	      table.insert(alerts, "DI"..id.."_read_closed_failed")
+      else
+        telemetry["di"..id.."_counter"] = counter
+        telemetry["di"..id.."_reset_time"] = reset_time
+      end
   end
   telemetry["status"] = status
   telemetry["alerts"] = alerts
