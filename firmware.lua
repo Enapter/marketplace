98685814bf752e0a11b7e9bb58168f5e679d8396
Rina-an <86365235+Rina-an@users.noreply.github.com>
2022-05-06 16:54:53 +0300

    Update firmware.lua

--- firmware.lua
+++ firmware.lua
@@ -25,23 +25,23 @@ function send_telemetry ()
   local status = "ok"
 
   for id = 1, 7 do
-    local di_is_closed, err = di7.is_closed(id)
+      local di_is_closed, err = di7.is_closed(id)
       if di_is_closed == nil then
-        status = "error"
+	      status = "error"
         enapter.log("Reading closed di"..id.." failed: "..di7.err_to_st(err))
 	      table.insert(alerts, "DI"..id.."_read_closed_failed")
       else
         telemetry["di"..id.."_closed"] = di_is_closed
       end
-    local di_is_opened, err = di7.is_opened(id)
+      local di_is_opened, err = di7.is_opened(id)
       if di_is_opened == nil then
-        status = "error"
+	      status = "error"
         enapter.log("Reading open di"..id.." failed: "..di7.err_to_st(err))
 	      table.insert(alerts, "DI"..id.."_read_closed_failed")
       else
         telemetry["di"..id.."_opened"] = di_is_opened
       end
-    local counter, reset_time, err = di7.read_counter(id)
+      local counter, reset_time, err = di7.read_counter(id)
       if counter == nil then
 	      status = "error"
         enapter.log("Reading counter di"..id.." failed: "..di7.err_to_st(err))
