14dcef69e5141dfd3c42c27b4ee22eeb6811f8f6
Rina-an <86365235+Rina-an@users.noreply.github.com>
2022-05-12 16:00:45 +0300

    Fixes fixes fixes

--- relays/di7_alerts/firmware.lua
+++ relays/di7_alerts/firmware.lua
@@ -12,24 +12,24 @@ function send_telemetry ()
   local alerts = {}
   local status = "ok"
   for id = 1, 7 do
-      local relay_status, err = di7.is_closed(id)
-      if relay_status ~= nil then
-        telemetry["di"..id.."_closed"] = relay_status
-        if relay_status == true then
-          table.insert(alerts,"DI"..id.."_closed_alert")
-        end
-      else
-        status = "error"
-        enapter.log("Reading closed di"..id.." failed: "..di7.err_to_str(err))
+    local relay_status, err = di7.is_closed(id)
+    if relay_status ~= nil then
+      telemetry["di"..id.."_closed"] = relay_status
+      if relay_status == true then
+        table.insert(alerts,"DI"..id.."_closed_alert")
       end
-      local counter, reset_time, err = di7.read_counter(id)
-      if counter ~= nil then
-        telemetry["di"..id.."_counter"] = counter
-        telemetry["di"..id.."_reset_time"] = reset_time
-      else
-        status = "error"
-        enapter.log("Reading counter di"..id.." failed: "..di7.err_to_str(err))
-      end
+    else
+      status = "error"
+      enapter.log("Reading closed di"..id.." failed: "..di7.err_to_str(err))
+    end
+    local counter, reset_time, err = di7.read_counter(id)
+    if counter ~= nil then
+      telemetry["di"..id.."_counter"] = counter
+      telemetry["di"..id.."_reset_time"] = reset_time
+    else
+      status = "error"
+      enapter.log("Reading counter di"..id.." failed: "..di7.err_to_str(err))
+    end
   end
   telemetry["status"] = status
   telemetry["alerts"] = alerts
