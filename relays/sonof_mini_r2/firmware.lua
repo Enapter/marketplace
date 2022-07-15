00857f665fa4e165fd187e54c520c01ad93f225d
Arina Andreeva <aandreeva@enapter.com>
2022-07-15 17:53:33 +0300

    Add blueprint for Sonoff MINI R2

---
+++ relays/sonof_mini_r2/firmware.lua
@@ -0,0 +1,329 @@
+json = require("json")
+
+-- Configuration variables must be also defined
+-- in `write_configuration` command arguments in manifest.yml
+IP_ADDRESS = 'ip_address'
+PORT = 'port'
+
+local ip_address
+local port
+local client
+
+-- Initiate device firmware. Called at the end of the file.
+function main()
+
+  config.init({
+    [IP_ADDRESS] = {type = 'string', required = true},
+    [PORT] = {type = 'string', required = true}
+  })
+
+  local sonoff, err = connect_sonoff()
+  if err then
+    enapter.log("[main]: can't connect to Sonoff: "..err, 'error')
+    enapter.send_telemetry({
+      status = 'connection_error',
+      alerts = { 'connection_error' }
+    })
+    return
+  else
+    ip_address = sonoff.ip_address
+    port = sonoff.port
+    client = sonoff.client
+
+    scheduler.add(30000, send_properties)
+    scheduler.add(1000, send_telemetry)
+
+    enapter.register_command_handler('turn_switch_on', command_turn_switch_on)
+    enapter.register_command_handler('wifi_signal_strength', command_wifi_signal_strength)
+  end
+end
+
+function send_properties()
+    enapter.send_properties({
+      vendor = 'Sonoff',
+      model = 'MINI R2'
+    })
+end
+
+-- holds global array of alerts that are currently active
+active_alerts = {}
+
+function send_telemetry()
+  local sonoff, err  = connect_sonoff()
+  if err then
+    enapter.log("Can't connect to Sonoff module: "..err)
+    enapter.send_telemetry({
+      status = 'no_data',
+      alerts = {'connection_err'}
+    })
+    return nill
+  else
+      enapter.send_properties({
+      ip_address =sonoff.ip_address,
+      port = sonoff.port
+    })
+    local snf_data = sonoff:get_device_info()
+    if next(snf_data) then
+      local telemetry = {}
+
+      telemetry.alerts = active_alerts
+      if telemetry.status == "on" then
+        active_alerts = { }
+      end
+      enapter.send_telemetry(telemetry)
+    else
+      enapter.send_telemetry({status = 'no_data', alerts = {'no_data'}})
+    end
+  end
+end
+
+
+-- holds global Sonoff connection
+local sonoff
+
+function connect_sonoff()
+  if sonoff then return sonoff, nil end
+
+  local values, err = config.read_all()
+  if err then
+    enapter.log('cannot read config: '..tostring(err), 'error')
+    return nil, 'cannot_read_config'
+  else
+    local ip_address, port = values[IP_ADDRESS], values[PORT]
+    if not ip_address or not port then
+      return nil, 'not_configured'
+    else
+      sonoff = Sonoff.new(ip_address, port)
+      return sonoff, nil
+    end
+  end
+end
+
+function has_value (tab, val)
+  for _, value in ipairs(tab) do
+    if value == val then
+      return true
+    end
+  end
+  return false
+end
+
+---------------------------------
+-- Stored Configuration API
+---------------------------------
+
+config = {}
+
+-- Initializes config options. Registers required UCM commands.
+-- @param options: key-value pairs with option name and option params
+-- @example
+--   config.init({
+--     address = { type = 'string', required = true },
+--     unit_id = { type = 'number', default = 1 },
+--     reconnect = { type = 'boolean', required = true }
+--   })
+function config.init(options)
+  assert(next(options) ~= nil, 'at least one config option should be provided')
+  assert(not config.initialized, 'config can be initialized only once')
+  for name, params in pairs(options) do
+    local type_ok = params.type == 'string' or params.type == 'number' or params.type == 'boolean'
+    assert(type_ok, 'type of `'..name..'` option should be either string or number or boolean')
+  end
+
+  enapter.register_command_handler('write_configuration', config.build_write_configuration_command(options))
+  enapter.register_command_handler('read_configuration', config.build_read_configuration_command(options))
+
+  config.options = options
+  config.initialized = true
+end
+
+-- Reads all initialized config options
+-- @return table: key-value pairs
+-- @return nil|error
+function config.read_all()
+  local result = {}
+
+  for name, _ in pairs(config.options) do
+    local value, err = config.read(name)
+    if err then
+      return nil, 'cannot read `'..name..'`: '..err
+    else
+      result[name] = value
+    end
+  end
+
+  return result, nil
+end
+
+-- @param name string: option name to read
+-- @return string
+-- @return nil|error
+function config.read(name)
+  local params = config.options[name]
+  assert(params, 'undeclared config option: `'..name..'`, declare with config.init')
+
+  local ok, value, ret = pcall(function()
+    return storage.read(name)
+  end)
+
+  if not ok then
+    return nil, 'error reading from storage: '..tostring(value)
+  elseif ret and ret ~= 0 then
+    return nil, 'error reading from storage: '..storage.err_to_str(ret)
+  elseif value then
+    return config.deserialize(name, value), nil
+  else
+    return params.default, nil
+  end
+end
+
+-- @param name string: option name to write
+-- @param val string: value to write
+-- @return nil|error
+function config.write(name, val)
+  local ok, ret = pcall(function()
+    return storage.write(name, config.serialize(name, val))
+  end)
+
+  if not ok then
+    return 'error writing to storage: '..tostring(ret)
+  elseif ret and ret ~= 0 then
+    return 'error writing to storage: '..storage.err_to_str(ret)
+  end
+end
+
+-- Serializes value into string for storage
+function config.serialize(_, value)
+  if value then
+    return tostring(value)
+  else
+    return nil
+  end
+end
+
+-- Deserializes value from stored string
+function config.deserialize(name, value)
+  local params = config.options[name]
+  assert(params, 'undeclared config option: `'..name..'`, declare with config.init')
+
+  if params.type == 'number' then
+    return tonumber(value)
+  elseif params.type == 'string' then
+    return value
+  elseif params.type == 'boolean' then
+    if value == 'true' then
+      return true
+    elseif value == 'false' then
+      return false
+    else
+      return nil
+    end
+  end
+end
+
+function config.build_write_configuration_command(options)
+  return function(ctx, args)
+    for name, params in pairs(options) do
+      if params.required then
+        assert(args[name], '`'..name..'` argument required')
+      end
+
+      local err = config.write(name, args[name])
+      if err then ctx.error('cannot write `'..name..'`: '..err) end
+    end
+  end
+end
+
+function config.build_read_configuration_command(_config_options)
+  return function(ctx)
+    local result, err = config.read_all()
+    if err then
+      ctx.error(err)
+    else
+      return result
+    end
+  end
+end
+
+---------------------------------
+-- Sonoff MINI R2 API
+---------------------------------
+
+Sonoff = {}
+
+function Sonoff.new(ip_address, port)
+  assert(type(ip_address) == 'string', 'ip_address (arg #1) must be string, given: '..inspect(ip_address))
+  assert(type(port) == 'string', 'port (arg #2) must be string, given: '..inspect(port))
+
+  local self = setmetatable({}, { __index = Sonoff })
+  self.ip_address = ip_address
+  self.port= port
+  self.client = http.client({timeout = 10})
+
+  Sonoff.ip_address= ip_address
+  Sonoff.port = port
+  return self
+end
+
+function Sonoff:get_device_info()
+  local body = json.encode({
+    data = {},
+    deviceid =''
+  })
+
+  local response, err = client:post('http://'..ip_address..':'..port..'/zeroconf/info', 'application/json', body)
+
+  if err then
+    enapter.log('Cannot do request: '..err, 'error')
+  elseif response.code ~= 200 then
+    enapter.log('Request returned non-OK code: '..response.code, 'error')
+  else
+    enapter.log('Request succeeded: '..response.body)
+    return response
+  end
+end
+
+-- Turn switch on and off
+
+function command_turn_switch_on(ctx, args)
+  if args['action'] then
+    local body = json.encode({
+      data = {switch = args['action']},
+      deviceid = ''
+    })
+
+  local response, err = client:post('http://'..ip_address..':'..port..'/zeroconf/switch', 'json',body)
+
+  if err then
+    ctx.error('Cannot do request: '..err, 'error')
+  elseif response.code ~= 200 then
+    ctx.error('Request returned non-OK code: '..response.code, 'error')
+  else
+    return json.decode(response.body)
+  end
+  return nil
+  end
+end
+
+--Get WiFI Signal info
+
+function command_wifi_signal_strength(ctx)
+    local body = json.encode({
+      data = {},
+      deviceid = ''
+    })
+
+  local response, err = client:post('http://'..ip_address..':'..port..'/zeroconf/signal_strength', 'json',body)
+
+  if err then
+    ctx.error('Cannot do request: '..err, 'error')
+  elseif response.code ~= 200 then
+    ctx.error('Request returned non-OK code: '..response.code, 'error')
+  else
+    return json.decode(response.body)
+  end
+  return nil
+end
+
+
+main()
