8e74891aa8395fbf1a0a44292f11fbc899d5d01a
Alexander Chirkov <achirkov@enapter.com>
2022-09-20 16:55:55 +0300

    feat(workflow): updated blueprints linter workflow

---
+++ .scripts/category_icon_checker.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+dirs=$(find ./ -type d -mindepth 1 -maxdepth 1 -not -name '.*' -exec basename {} \;)
+
+for dir in $dirs
+do
+  printf "Checking %s..." "${dir}"
+  code=$(curl -sI -o/dev/null -w'%{http_code}' https://static.enapter.com/marketplace-categories/"${dir}".svg)
+  if [ "$code" == "200" ]; then
+    echo "ok"
+  else
+    echo "failed"
+    echo "::error::icon for category ${dir} not found"
+    exit 1
+  fi
+done
