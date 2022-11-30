9fc4b0aab63e13a6be39119bc10ce1037674b091
Nikolay V. Krasko <nikolay@enapter.com>
2022-11-30 19:13:13 +0400

    adopting blueprint to rl api

--- relays/ucm_kit_rl1_impulse_push_button/README.md
+++ relays/ucm_kit_rl1_impulse_push_button/README.md
@@ -9,7 +9,7 @@ This Blueprint is specially designed for makers, students and researchers to tes
 ## Connect to Enapter
 
 - Sign up to the Enapter Cloud using the [Web](https://cloud.enapter.com/) or mobile app ([iOS](https://apps.apple.com/app/id1388329910), [Android](https://play.google.com/store/apps/details?id=com.enapter&hl=en)).
-- Use the [Enapter ENP-RL6](https://go.enapter.com/handbook-enp-rl6) module for physical connection. See [connection instructions](https://go.enapter.com/handbook-enp-rl6-conn) in the module manual.
+- Use the [Enapter UCM Kit RL1](https://go.enapter.com/ucm-kit-rl1) module for physical connection.
 - [Add ENP-KIT-RL1 to your site](https://go.enapter.com/handbook-mobile-app) using the mobile app.
 - [Upload](https://go.enapter.com/developers-upload-blueprint) this blueprint to ENP-RL6.
 
--- relays/ucm_kit_rl1_impulse_push_button/firmware.lua
+++ relays/ucm_kit_rl1_impulse_push_button/firmware.lua
@@ -7,12 +7,8 @@ end
 function send_telemetry()
   local telemetry = { alerts = {} }
 
-  local is_powered, err = is_load_powered_on()
-  if err then
-    enapter.log(err, 'error')
-    telemetry['status'] = 'error'
-    telemetry['alerts'] = {'cannot_read_relay'}
-  elseif is_powered then
+  local is_powered = rl.is_closed()
+  if is_powered then
     telemetry['status'] = 'on'
   else
     telemetry['status'] = 'off'
@@ -29,17 +25,9 @@ IMPULSE_DURATION = 1000
 
 function impulse(ctx)
   local ret = rl.impulse(IMPULSE_DURATION)
-  if ret ~= 0 then
+  if ret ~= nil then
     ctx.error('Cannot impulse for '..IMPULSE_DURATION..' ms relay')
   end
-end
-
-function is_load_powered_on()
-  local is_closed, ret = rl.is_closed()
-  if ret ~= 0 then
-    return nil, 'Cannot read relay status'
-  end
-  return is_closed, nil
 end
 
 main()
