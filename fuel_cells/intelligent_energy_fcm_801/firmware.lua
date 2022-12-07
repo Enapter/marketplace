e9b9928affe1a5ac1b93ddbed00506c293e6c8c2
Nikita Afanasenko <nikita@afanasenko.name>
2022-12-07 16:25:45 +0300

    Update IE FC blueprints (#142)
    * fix(ie-fc): Do not send alerts if no CAN messages received
    
    * feat(ie-fc): Support CAN index and debug mode config in IE FC
    
    * feat(ie-fc): Support relay channel ID config for IE FC RL6

--- fuel_cells/intelligent_energy_fcm_801/firmware.lua
+++ fuel_cells/intelligent_energy_fcm_801/firmware.lua
@@ -1,3 +1,8 @@
+local config = require('enapter.ucm.config')
+
+INDEX_CONFIG = "index"
+SAVE_0x400_CONFIG = "save_0x400"
+
 VENDOR = "Intelligent Energy"
 MODEL = "FCM 801"
 
@@ -5,8 +10,15 @@ telemetry = {}
 alerts = {}
 total_can_packets = 0
 is_serial_number_completed = false
-serial_number = ""
+serial_number = nil
+fw_ver = nil
 temp_serial_number = ""
+alerts_ab_received = false
+alerts_d_received = false
+
+can_index = 1
+save_0x400 = false
+messages_0x400 = {}
 
 function main()
   local result = can.init(500, can_handler)
@@ -16,9 +28,16 @@ function main()
 
   scheduler.add(30000, send_properties)
   scheduler.add(1000, send_telemetry)
-  -- Alerts are sent by FC once per second,
-  -- so we use the bigger interval on our side.
-  scheduler.add(2000, send_alerts)
+
+  config.init({
+    [INDEX_CONFIG] = { type = "number", default = 1, required = true },
+    [SAVE_0x400_CONFIG] = { type = "boolean", default = false }
+  })
+  -- Store index in memory to prevent reading it from flash on every CAN message
+  scheduler.add(5000, function()
+    can_index = config.read(INDEX_CONFIG) or 1
+    save_0x400 = config.read(SAVE_0x400_CONFIG) or false
+  end)
 end
 
 function send_properties()
@@ -28,19 +47,34 @@ function send_properties()
     info["serial_number"] = serial_number
     is_serial_number_completed = false
   end
+  info["fw_ver"] = fw_ver
 
   enapter.send_properties(info)
 end
 
 function send_telemetry()
   telemetry["total_can_packets"] = total_can_packets
+
+  local str_0x400
+  for _,data in pairs(messages_0x400) do
+    str_0x400 = "" or str_0x400
+    str_0x400 = str_0x400.." "..
+      string.format("%02x%02x%02x%02x%02x%02x%02x%02x", string.unpack("I1I1I1I1I1I1I1I1", data))
+  end
+  telemetry["messages_0x400"] = str_0x400
+  messages_0x400 = {}
+
+  -- Make sure to add alerts only if it was really reported by the FC
+  -- to avoid accidental cleaning of alerts in the Cloud
+  if alerts_ab_received and alerts_d_received then
+    telemetry.alerts = alerts
+    alerts = {}
+    alerts_ab_received = false
+    alerts_d_received = false
+  end
+
   enapter.send_telemetry(telemetry)
   telemetry = {}
-end
-
-function send_alerts()
-  enapter.send_telemetry({ alerts = alerts })
-  alerts = {}
 end
 
 function can_handler(msg_id, data)
@@ -49,7 +83,9 @@ function can_handler(msg_id, data)
   local rx_scale_factor10 = 10.0
   local rx_scale_factor100 = 100.0
 
-  if msg_id == 0x310 then
+  if msg_id == (0x400 + can_index - 1) and save_0x400 then
+    table.insert(messages_0x400, data)
+  elseif msg_id == (0x310 + can_index - 1) then
     if not is_serial_number_completed then
       if string.byte(data, 1) > 127 then
         if #temp_serial_number == 8 then
@@ -64,36 +100,37 @@ function can_handler(msg_id, data)
         temp_serial_number = string.char(string.byte(data, 1, 8))
       end
     end
-  elseif msg_id == 0x318 then
-    telemetry["software_version"] = software_version(data)
-  elseif msg_id == 0x320 then
+  elseif msg_id == (0x318 + can_index - 1) then
+    fw_ver = software_version(data)
+  elseif msg_id == (0x320 + can_index - 1) then
     telemetry["run_hours"] = touint32(string.sub(data, 1, 4))
     telemetry["total_run_energy"] = touint32(string.sub(data, 5, 8))
-  elseif msg_id == 0x328 then
+  elseif msg_id == (0x328 + can_index - 1) then
     telemetry["fault_flags_a"] = touint32(string.sub(data, 1, 4))
     telemetry["fault_flags_b"] = touint32(string.sub(data, 5, 8))
     local flag_a = flag_a_error(touint32(string.sub(data, 1, 4)))
     local flag_b = flag_b_error(touint32(string.sub(data, 5, 8)))
     alerts = table.move(flag_a, 1, #flag_a, 1, alerts)
     alerts = table.move(flag_b, 1, #flag_b, #alerts + 1, alerts)
-  elseif msg_id == 0x338 then
+    alerts_ab_received = true
+  elseif msg_id == (0x338 + can_index - 1) then
     telemetry["watt"] = toint16(string.sub(data, 1, 2))
     telemetry["volt"] = toint16(string.sub(data, 3, 4)) / rx_scale_factor100
     telemetry["amp"] = toint16(string.sub(data, 5, 6)) / rx_scale_factor100
     telemetry["anode_pressure"] = toint16(string.sub(data, 7, 8)) / rx_scale_factor10
-  elseif msg_id == 0x348 then
+  elseif msg_id == (0x348 + can_index - 1) then
     telemetry["outlet_temp"] = toint16(string.sub(data, 1, 2)) / rx_scale_factor100
     telemetry["inlet_temp"] = toint16(string.sub(data, 3, 4)) / rx_scale_factor100
     telemetry["dcdc_volt_setpoint"] = toint16(string.sub(data, 5, 6)) / rx_scale_factor100
     telemetry["dcdc_amp_limit"] = toint16(string.sub(data, 7, 8)) / rx_scale_factor100
-  elseif msg_id == 0x358 then
+  elseif msg_id == (0x358 + can_index - 1) then
     telemetry["louver_pos"] = toint16(string.sub(data, 1, 2)) / rx_scale_factor100
     telemetry["fan_sp_duty"] = toint16(string.sub(data, 3, 4)) / rx_scale_factor100
-  elseif msg_id == 0x368 then
-    telemetry["state"] = parse_state(string.byte(data, 1))
-    telemetry["load_logic"] = string.byte(data, 2)
-    telemetry["out_bits"] = string.byte(data, 3)
-  elseif msg_id == 0x378 then
+  elseif msg_id == (0x368 + can_index - 1) then
+    telemetry["status"] = parse_state(string.byte(data, 1))
+    -- telemetry["load_logic"] = string.byte(data, 2)
+    -- telemetry["out_bits"] = string.byte(data, 3)
+  elseif msg_id == (0x378 + can_index - 1) then
     local flag = touint32(string.sub(data, 1, 4))
 
     if flag & 0x00200000 ~= 0 then
@@ -106,6 +143,7 @@ function can_handler(msg_id, data)
     local flag_d = flag_d_error(touint32(string.sub(data, 5, 8)))
 
     alerts = table.move(flag_d, 1, #flag_d, #alerts + 1, alerts)
+    alerts_d_received = true
   end
 end
 
