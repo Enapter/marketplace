76e8977e2c908739b4ea42f50bfccfc1e7318ad1
Tatyana <tyugaj@enapter.com>
2023-02-15 18:35:05 +0100

    (blueprint) Add MPP solar (parallel mode)

---
+++ solar_inverters/mpp_solar_parallel/mpp_solar_fw/main.lua
@@ -0,0 +1,99 @@
+local mpp_solar = require("mpp_solar")
+local parser = require("parser")
+
+function main()
+    local err = rs232.init(mpp_solar.baudrate, mpp_solar.data_bits, mpp_solar.parity, mpp_solar.stop_bits)
+    if err ~= 0 then
+        enapter.log('RS232 init failed: '..rs232.err_to_str(err), 'error')
+        enapter.send_telemetry({ status = 'error', alerts = {'init_error'}})
+        return
+    end
+
+    scheduler.add(30000, send_properties)
+    scheduler.add(1000, send_telemetry)
+
+    enapter.register_command_handler("get_parallel_info", command_get_parallel_device_info)
+end
+
+local max_parallel_number = nil
+
+function send_properties()
+    local properties = {}
+
+    max_parallel_number = parser:get_max_parallel_number()
+
+    if not max_parallel_number then
+        enapter.send_properties(properties)
+        return
+    else
+      properties["max_parallel_num"] = max_parallel_number
+
+      local data, err = parser:get_device_model()
+      if data then
+          properties["model"] = data
+      else
+          enapter.log("Can not get device model: "..err, 'error')
+      end
+
+      local data, err = parser:get_firmware_version()
+      if data then
+          properties["fw_ver"] = data
+      else
+          enapter.log("Can not get device firmware version: "..err, 'error')
+      end
+
+      local data, err = parser:get_protocol_version()
+      if data then
+          properties["protocol_ver"] = data
+      else
+          enapter.log("Can not get device protocol version: "..err, 'error')
+      end
+
+      enapter.send_properties(properties)
+    end
+end
+
+function send_telemetry()
+    local telemetry = {}
+
+    if not max_parallel_number then
+        enapter.send_telemetry({status = 'no_data', alerts = {'no_data'}})
+        return
+    elseif max_parallel_number == 0 then
+        enapter.send_telemetry({status = 'no_data', alerts = {'single_mode'}})
+        return
+    else
+        local data, err = parser:get_all_parallel_info(max_parallel_number)
+        if data then
+            merge_tables(telemetry, data)
+
+            if #telemetry["alerts"] > 0 then
+              telemetry["status"] = 'error'
+            else
+              telemetry["status"] = 'ok'
+            end
+
+            enapter.send_telemetry(telemetry)
+        else
+            enapter.log("Failed to get parallel info: "..err, 'error')
+            enapter.send_telemetry({status = 'no_data', alerts = {'no_data'}})
+            return
+        end
+    end
+end
+
+function command_get_parallel_device_info(ctx, args)
+  if args["device"] then
+    return parser:get_parallel_info(math.floor(args["device"]))
+  else
+    ctx.error("No device's number in arguments")
+  end
+end
+
+function merge_tables(t1, t2)
+    for key, value in pairs(t2) do
+        t1[key] = value
+    end
+end
+
+main()
---
+++ solar_inverters/mpp_solar_parallel/mpp_solar_fw/parser.lua
@@ -0,0 +1,193 @@
+local mpp_solar = require("mpp_solar")
+local commands = require("commands")
+
+local device_rating_info = commands.device_rating_info
+local firmware_version = commands.firmware_version
+local device_protocol = commands.device_protocol
+local device_mode = commands.device_mode
+local output_mode = commands.output_mode
+local priorities = commands.set_priorities
+local parallel_info = commands.parallel_info
+
+local parser = {}
+
+function parser:get_device_model()
+    local result, data = mpp_solar:run_with_cache(device_rating_info.command)
+    if result then
+        return split(data)[device_rating_info.data.ac_out_apparent_power].."VA", nil
+    else
+        return nil, 'no_data'
+    end
+end
+
+function parser:get_firmware_version()
+    local result, data = mpp_solar:run_with_cache(firmware_version.command)
+    if result then
+      return data, nil
+    else
+      return nil, 'no_data'
+    end
+end
+
+function parser:get_protocol_version()
+    local result, data = mpp_solar:run_with_cache(device_protocol.command)
+    if result then
+      return data, nil
+    else
+      return nil, 'no_data'
+    end
+end
+
+function parser:get_device_rating_info()
+    local data = mpp_solar:run_command(device_rating_info.command)
+    local telemetry = {}
+    if data then
+      for name, index in pairs(device_rating_info.data) do
+        telemetry[name] = tonumber(split(data)[index])
+      end
+
+      return telemetry, nil
+    else
+      return nil, 'no_data'
+    end
+end
+
+function parser:get_all_parallel_info(devices_number)
+    local telemetry = {}
+    local alerts = {}
+    local total_pv_input_power = 0
+    local non_existing_devices = 0
+
+    for i = 0, devices_number do
+      local data = parser:get_parallel_info(i)
+      if data then
+        if data["fault_code_"..i] then
+          table.insert(alerts, data["fault_code_"..i])
+        end
+
+        if data["pv_input_power_"..i] ~= nil then
+          total_pv_input_power = total_pv_input_power + data["pv_input_power_"..i]
+        end
+
+        for name, _ in pairs(parallel_info.data.total) do
+          telemetry[name] = data[name]
+        end
+      else
+        non_existing_devices = non_existing_devices + 1
+      end
+    end
+
+    if non_existing_devices == devices_number then
+      return nil, 'no_data'
+    else
+      telemetry["total_pv_input_power"] = total_pv_input_power
+      telemetry["alerts"] = alerts
+      return telemetry, nil
+    end
+end
+
+function parser:get_parallel_info(device_number)
+    local data = mpp_solar:run_command(parallel_info.command..device_number)
+
+    if data then
+      data = split(data)
+
+      if data[parallel_info.data.general.num.parallel_num_exists] == '1' then
+        local telemetry = {}
+        for name, index in pairs(parallel_info.data.general.num) do
+          telemetry[name.."_"..device_number] = tonumber(data[index])
+        end
+
+        for name, index in pairs(parallel_info.data.general.str) do
+          telemetry[name.."_"..device_number] = data[index]
+        end
+
+        telemetry["work_mode_"..device_number] = parser:get_device_mode(
+          telemetry["work_mode_"..device_number]
+        )
+
+        telemetry["output_mode_"..device_number] = parser:get_output_mode(
+          telemetry["output_mode_"..device_number]
+        )
+
+        local pv_input_volt = telemetry["pv_input_volt_"..device_number]
+        local pv_input_amp = telemetry["pv_input_amp_"..device_number]
+        if pv_input_volt and pv_input_amp then
+          telemetry["pv_input_power_"..device_number] = pv_input_volt * pv_input_amp
+        end
+
+        telemetry["fault_code_"..device_number] = parser:get_parallel_device_alerts(
+          telemetry["fault_code_"..device_number]
+        )
+
+        telemetry["charger_source_priority_"..device_number] = parser:get_charger_priority(
+          telemetry["charger_source_priority_"..device_number]
+        )
+
+        for name, index in pairs(parallel_info.data.total) do
+          telemetry[name] = tonumber(data[index])
+        end
+
+        return telemetry
+      else
+        return nil, 'no_device_with_such_number'
+      end
+    else
+      return nil, 'no_data'
+    end
+end
+
+function parser:get_parallel_device_alerts(value)
+  if value then
+    return parallel_info.fault_codes[value]
+  else
+    return nil
+  end
+end
+
+function parser:get_charger_priority(priority)
+    for name, value in pairs(priorities.charger.values) do
+      if value == priority then
+        return name
+      end
+    end
+end
+
+function parser:get_device_mode(value)
+    if not value then
+      return 'unknown'
+    else
+      return device_mode.values[value]
+    end
+end
+
+function parser:get_output_mode(value)
+    return output_mode.values[value]
+end
+
+function parser:get_max_parallel_number()
+    local result, data = mpp_solar:run_with_cache(device_rating_info.command)
+    if result then
+      local max_parallel_number = split(data)[device_rating_info.data.parallel_max_num]
+      if max_parallel_number == '-' then
+        return 0
+      else
+        return tonumber(max_parallel_number)
+      end
+    end
+end
+
+function split(str, sep)
+    if sep == nil then
+        sep = "%s"
+    end
+
+    local t = {}
+    for part in string.gmatch(str, "([^"..sep.."]+)") do
+        table.insert(t, part)
+    end
+
+    return t
+end
+
+return parser
