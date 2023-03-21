d55b944d95b67fdf2724b910d683201860f827d1
Tatyana <tyugaj@enapter.com>
2023-03-21 16:50:17 +0100

    (blueprint) update Pylontech BESS

--- battery_management_systems/pylontech_us2000_rs485/firmware.lua
+++ battery_management_systems/pylontech_us2000_rs485/firmware.lua
@@ -1,9 +1,27 @@
--- Pylontech US2000 Battery
--- Serial over RS485
--- Baud rate = 9600 bps, 8 data bits, no parity, 1 stop bit
+local config = require('enapter.ucm.config')
+
+BAUDRATE_CONFIG = 'baudrate'
+CELLS_CONFIG = 'cells'
+GROUP_CONFIG = 'group'
+
+local BAUDRATE, CELL_COUNT, GROUP_COUNT
+local DATA_BITS, PARITY, STOP_BITS = 8, 'N', 1
 
 function main()
-  local result = rs485.init(9600, 8, "N", 1)
+  config.init({
+    [BAUDRATE_CONFIG] = { type = 'number', required = true, default = 115200 },
+    [CELLS_CONFIG] = { type = 'number', required = true, default = 8 },
+    [GROUP_CONFIG] = { type = 'number', required = true, default = 0 },
+  })
+
+  local values, err = config.read_all()
+  if err then
+    enapter.log('cannot read config: '..tostring(err), 'error')
+  else
+    BAUDRATE = values[BAUDRATE_CONFIG]
+  end
+
+  local result = rs485.init(BAUDRATE, DATA_BITS, PARITY, STOP_BITS)
   if result ~= 0 then
     enapter.log("RS485 init failed: "..rs485.err_to_str(result))
   end
@@ -13,35 +31,26 @@ function main()
 end
 
 function properties()
-  enapter.send_properties({ vendor = "Pylontech", model = "US2000" })
+  enapter.send_properties({
+    vendor = "Pylontech",
+    baudrate = BAUDRATE
+  })
 end
 
 function telemetry()
-  local b0_TotalCurrent, b0_TotalVoltage, b0_RemainingCapacity, b0_TotalCapacity = get_analog_value(0)
-  local b1_TotalCurrent, b1_TotalVoltage, b1_RemainingCapacity, b1_TotalCapacity = get_analog_value(1)
-  local b2_TotalCurrent, b2_TotalVoltage, b2_RemainingCapacity, b2_TotalCapacity = get_analog_value(2)
-  local b3_TotalCurrent, b3_TotalVoltage, b3_RemainingCapacity, b3_TotalCapacity = get_analog_value(3)
-
-  if b0_TotalCurrent then
-    enapter.send_telemetry({
-      b0_total_current = b0_TotalCurrent,
-      b0_total_voltage = b0_TotalVoltage,
-      b0_total_power = b0_TotalCurrent * b0_TotalVoltage,
-      b0_battery_level = b0_RemainingCapacity / b0_TotalCapacity * 100,
-      b1_total_current = b1_TotalCurrent,
-      b1_total_voltage = b1_TotalVoltage,
-      b1_total_power = b1_TotalCurrent * b1_TotalVoltage,
-      b1_battery_level = b1_RemainingCapacity / b1_TotalCapacity * 100,
-      b2_total_current = b2_TotalCurrent,
-      b2_total_voltage = b2_TotalVoltage,
-      b2_total_power = b2_TotalCurrent * b2_TotalVoltage,
-      b2_battery_level = b2_RemainingCapacity / b2_TotalCapacity * 100,
-      b3_total_current = b3_TotalCurrent,
-      b3_total_voltage = b3_TotalVoltage,
-      b3_total_power = b3_TotalCurrent * b3_TotalVoltage,
-      b3_battery_level = b3_RemainingCapacity / b3_TotalCapacity * 100
-    })
+  local values, err = config.read_all()
+  if err then
+    enapter.log('cannot read config: '..tostring(err), 'error')
+  else
+    CELL_COUNT = values[CELLS_CONFIG]
   end
+
+  local telemetry = {}
+  for cell = 0, CELL_COUNT do
+    merge_tables(telemetry, get_analog_value(cell))
+  end
+
+  enapter.send_telemetry(telemetry)
 end
 
 function get_analog_value(addr)
@@ -58,34 +67,49 @@ function get_analog_value(addr)
 
   local binary_data = fromhex(data:sub(14, -6))
 
-  local CellsCount = binary_data:byte(3)
-  local TempCount = binary_data:byte(CellsCount * 2 + 4)
+  local data = {}
+  CELL_COUNT = binary_data:byte(3)
+  GROUP_COUNT = binary_data:byte(CELL_COUNT * 2 + 4)
+  local data_pos = CELL_COUNT * 2 + GROUP_COUNT * 2 + 9
 
-  local data_pos = CellsCount * 2 + TempCount * 2 + 9
-  local TotalCurrent = get_int2_complement(binary_data:sub(data_pos, data_pos + 1)) / 100.0
-  data_pos = data_pos + 2
-  local TotalVoltage = get_int2(binary_data:sub(data_pos, data_pos + 1)) / 100.0
-  data_pos = data_pos + 2
-  local RemainingCapacity = get_int2(binary_data:sub(data_pos, data_pos + 1)) / 100.0
-  data_pos = data_pos + 2
-  local TotalCapacity = get_int2(binary_data:sub(data_pos, data_pos + 1)) / 100.0
-  data_pos = data_pos + 2
-  local Cycles = get_int2(binary_data:sub(data_pos, data_pos + 1))
+  data['total_current_'..addr] = get_int2_complement(binary_data:sub(data_pos, data_pos + 1)) / 100.0
 
-  return TotalCurrent, TotalVoltage, RemainingCapacity, TotalCapacity, Cycles
+  data_pos = data_pos + 2
+  data['total_voltage'..addr] = get_int2(binary_data:sub(data_pos, data_pos + 1)) / 100.0
+
+  data['total_power_'..addr] = data['total_voltage'..addr] * data['total_current_'..addr]
+
+  data_pos = data_pos + 2
+  data['remaining_capacity_'..addr] = get_int2(binary_data:sub(data_pos, data_pos + 1)) / 100.0
+
+  data_pos = data_pos + 2
+  data['total_capacity_'..addr] = get_int2(binary_data:sub(data_pos, data_pos + 1)) / 100.0
+
+  data['battery_level_'..addr] = data['remaining_capacity_'..addr] / data['total_capacity_'..addr]
+
+  -- data_pos = data_pos + 2
+  -- data['cycles_'..addr] = get_int2(binary_data:sub(data_pos, data_pos + 1))
+
+  return data
 end
 
 function make_message(addr, cid2, command)
+  local SOI = "\x7E" -- ~
+  local VER = "\x20"
+  local EOI = "\x0D" -- carriage return
   local len = 0
+
   if command then
     len = 2
   end
-  local message = "\x20" .. string.char(addr) .. "\x4A" .. string.char(cid2) .. get_length(len)
+
+  local message = VER .. string.char(addr) .. "\x4A" .. string.char(cid2) .. get_length(len)
   if command then
     message = message .. string.char(command)
   end
+
   local payload = to_ascii(message)
-  payload = "\x7E" .. payload .. get_checksum(payload) .. "\x0D"
+  payload = SOI .. payload .. get_checksum(payload) .. EOI
   return payload
 end
 
@@ -143,27 +167,10 @@ function get_length(value)
   return string.pack(">i2", val)
 end
 
---[[function hex_dump(str)
-    local len = string.len(str)
-    local dump = ""
-    local hex = ""
-    local asc = ""
-    for i = 1, len do
-        if 1 == i % 8 then
-            dump = dump .. hex .. asc .. "\n"
-            hex = string.format("%04x: ", i - 1)
-            asc = ""
-        end
-        local ord = string.byte(str, i)
-        hex = hex .. string.format("%02x ", ord)
-        if ord >= 32 and ord <= 126 then
-            asc = asc .. string.char(ord)
-        else
-            asc = asc .. "."
-        end
+function merge_tables(t1, t2)
+    for key, value in pairs(t2) do
+        t1[key] = value
     end
-    return dump .. hex
-            .. string.rep(" ", 8 - len % 8) .. asc
-end]]--
+end
 
 main()
