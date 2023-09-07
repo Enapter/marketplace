538a4ba466bb889cc1de66bedb34c2be3e3aed98
Aleksey Bakin <alexey.bakin@gmail.com>
2023-09-07 10:26:28 +0300

    feat(mpp-solar): support watchpower rules

--- solar_inverters/mpp_solar/src/main.lua
+++ solar_inverters/mpp_solar/src/main.lua
@@ -1,6 +1,7 @@
 local mpp_solar = require('mpp_solar')
 local parser = require('parser')
 local commands = require('commands')
+local rules = require('rules')
 
 function main()
   local err =
@@ -11,11 +12,18 @@ function main()
     return
   end
 
+  err = rules:load()
+  if err then
+    enapter.log('rules init failed: ' .. err, 'error')
+  end
+
   scheduler.add(30000, send_properties)
   scheduler.add(1000, send_telemetry)
+  scheduler.add(10000, execute_rules)
 
   enapter.register_command_handler('set_output_priority', command_set_output_priority)
   enapter.register_command_handler('set_charger_priority', command_set_charger_priority)
+  enapter.register_command_handler('set_rules', command_set_rules)
 end
 
 local max_parallel_number = nil
@@ -112,6 +120,10 @@ function send_telemetry()
   collectgarbage()
 end
 
+function execute_rules()
+  rules:execute()
+end
+
 function command_set_charger_priority(ctx, args)
   command_set_priority(ctx, args, commands.set_priorities.charger)
 end
@@ -135,6 +147,96 @@ function command_set_priority(ctx, args, priorities)
   if not result then
     ctx.error(err)
   end
+end
+
+function command_set_rules(ctx, args)
+  local args_rules = args['rules']
+  if not args_rules then
+    ctx.error("missed required argument 'rules'")
+  end
+
+  local tz_offset = args['tz_offset']
+  local tz_offsets = args['tz_offsets']
+
+  if tz_offset and tz_offsets then
+    ctx.error("only one argument should pass 'tz_offset' or 'tz_offsets")
+  end
+
+  if tz_offset then
+    tz_offsets = { { from = 0, offset = tz_offset } }
+  end
+
+  local parsed_rules, err = convert_and_validate_args_rules(args_rules)
+  if err then
+    ctx.error(err)
+  end
+
+  local err = rules:set(parsed_rules, tz_offsets)
+  if err then
+    ctx.error(err)
+  end
+end
+
+function convert_and_validate_args_rules(args_rules)
+  if args_rules == nil then
+    return {}
+  end
+
+  if type(args_rules) ~= 'table' then
+    return nil, "invalid type of 'rules', should be a 'table'"
+  end
+
+  local converted_rules = {}
+  for i, r in ipairs(args_rules) do
+    local action = r.action
+    if not action then
+      action = {}
+    end
+
+    local priorities
+    if action.name == 'set_charger_priority' then
+      priorities = commands.set_priorities.charger
+    elseif action.name == 'set_output_priority' then
+      priorities = commands.set_priorities.output
+    else
+      return nil, 'action is missed at rule #' .. i
+    end
+
+    local priority
+    if action.arguments and action.arguments.priority then
+      priority = action.arguments.priority
+    else
+      return nil, 'priority is missed at rule #' .. i
+    end
+
+    local priority_value = priorities.values[priority]
+    if not priority_value then
+      return nil, 'unsupported priority value at rule #' .. i
+    end
+
+    local condition = r.condition
+    if not condition then
+      return nil, 'condition is missed at rule #' .. i
+    end
+
+    if not condition.voltage_min and not condition.voltage_max then
+      return nil, 'condition on voltage is missed at rule #' .. i
+    end
+
+    if
+      (condition.time_min and not condition.time_max)
+      or (condition.time_max and not condition.time_min)
+    then
+      return nil, 'condition on time should have both min and max at rule #' .. i
+    end
+
+    converted_rules[i] = {
+      cmd = priorities.cmd .. priority_value,
+      condition = condition,
+    }
+  end
+
+  return converted_rules
 end
 
 function merge_tables(t1, t2)
