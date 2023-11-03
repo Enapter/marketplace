2173d603b3401dc57ec81bc38ed22eb302184d65
Tatyana Yugaj <58917801+anataty@users.noreply.github.com>
2023-11-03 14:11:07 +0100

    feat(Alicat Flow meters): add CODA series (#272)

---
+++ flow_meters/alicat_coda_flow_meters_rs485_modbus/README.md
@@ -0,0 +1,21 @@
+# Alicat Scientific Mass Flow Meters CODA-Series
+
+This [Enapter Device Blueprint](https://go.enapter.com/marketplace-readme) integrates series of **Alicat Scientific CODA Flow Meters** - Coriolis mass flow meters which monitor mixed gases in changing process conditions via [ModBus RTU](https://go.enapter.com/developers-enapter-modbus) over [RS-485](https://go.enapter.com/developers-enapter-rs485). The device also supports [RS-232 communication interface](https://go.enapter.com/developers-enapter-rs232).
+
+## Supported models
+
+- K-Series;
+- KC-Series;
+- KF-Series;
+- KG-Series;
+
+## Connect to Enapter
+
+- Sign up to the Enapter Cloud using the [Web](https://cloud.enapter.com/) or mobile app ([iOS](https://apps.apple.com/app/id1388329910), [Android](https://play.google.com/store/apps/details?id=com.enapter&hl=en)).
+- Use the [Enapter ENP-RS485](https://go.enapter.com/handbook-enp-rs485) module for physical connection. See [connection instructions](https://go.enapter.com/handbook-enp-rs485-conn) in the module manual.
+- [Add ENP-RS485 to your site](https://go.enapter.com/handbook-mobile-app) using the mobile app.
+- [Upload](https://go.enapter.com/developers-upload-blueprint) this blueprint to ENP-RS485.
+
+## References
+
+- [Alicat CODA Coriolis Meters products page](https://go.enapter.com/alicat-coda-flow-meter-product)
---
+++ flow_meters/alicat_coda_flow_meters_rs485_modbus/firmware.lua
@@ -0,0 +1,108 @@
+local config = require('enapter.ucm.config')
+
+-- Configuration variables must be also defined
+-- in `write_configuration` command arguments in manifest.yml
+ADDRESS = 'address'
+BAUDRATE = 'baudrate'
+STOP_BITS = 'stop_bits'
+PARITY = 'parity'
+
+function main()
+  config.init({
+    [BAUDRATE] = { type = 'number', required = true, default = 19200 },
+    [STOP_BITS] = { type = 'number', required = true, default = 1 },
+    [PARITY] = { type = 'string', required = true, default = 'N' },
+    [ADDRESS] = { type = 'number', required = true, default = 1 },
+  })
+  scheduler.add(30000, send_properties)
+  scheduler.add(1000, send_telemetry)
+end
+
+function send_properties()
+  enapter.send_properties({
+    vendor = 'Alicat Scientific',
+  })
+end
+
+function send_telemetry()
+  local values, err = config.read_all()
+  if err ~= nil then
+    enapter.send_telemetry({
+      status = 'warning',
+      alerts = { 'config_read_error' },
+    })
+    return
+  end
+
+  local result = rs485.init(values[BAUDRATE], 8, values[PARITY], values[STOP_BITS])
+  if result ~= 0 then
+    enapter.log('RS485 init failed: ' .. result .. ' ' .. rs485.err_to_str(result), 'error', true)
+    enapter.send_telemetry({
+      status = 'warning',
+      alerts = { 'rs485_error' },
+    })
+    return
+  end
+
+  local address = values[ADDRESS]
+  local telemetry = {}
+  local status = 'ok'
+
+  local data, result = modbus.read_holdings(address, 1119, 2, 1000)
+  if data then
+    telemetry['proportional_gain'] = tofloat(data)
+  else
+    enapter.log('Register 1120 reading failed: ' .. modbus.err_to_str(result), 'error')
+    status = 'read_error'
+  end
+
+  local data, result = modbus.read_holdings(address, 1121, 2, 1000)
+  if data then
+    telemetry['integral_gain'] = tofloat(data)
+  else
+    enapter.log('Register 1122 reading failed: ' .. modbus.err_to_str(result), 'error')
+    status = 'read_error'
+  end
+
+  local data, result = modbus.read_holdings(address, 1123, 2, 1000)
+  if data then
+    telemetry['derivative_gain'] = tofloat(data)
+  else
+    enapter.log('Register 1124 reading failed: ' .. modbus.err_to_str(result), 'error')
+    status = 'read_error'
+  end
+
+  local data, result = modbus.read_inputs(address, 1208, 2, 1000)
+  if data then
+    telemetry['mass_flow'] = tofloat(data)
+  else
+    enapter.log('Register 1209 reading failed: ' .. modbus.err_to_str(result), 'error')
+    status = 'read_error'
+  end
+
+  local data, result = modbus.read_inputs(address, 1206, 2, 1000)
+  if data then
+    telemetry['volumetric_flow'] = tofloat(data)
+  else
+    enapter.log('Register 1207 reading failed: ' .. modbus.err_to_str(result), 'error')
+    status = 'read_error'
+  end
+
+  local data, result = modbus.read_inputs(address, 1204, 2, 1000)
+  if data then
+    telemetry['flow_temp'] = tofloat(data)
+  else
+    enapter.log('Register 1205 reading failed: ' .. modbus.err_to_str(result), 'error')
+    status = 'read_error'
+  end
+
+  telemetry['status'] = status
+  enapter.send_telemetry(telemetry)
+end
+
+function tofloat(register)
+  local raw_str = string.pack('BBBB', register[1] >> 8, register[1] & 0xff, register[2] >> 8, register[2] & 0xff)
+  return string.unpack('>f', raw_str)
+end
+
+main()
