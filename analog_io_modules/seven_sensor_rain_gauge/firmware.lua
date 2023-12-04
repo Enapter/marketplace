0b4137110ef4e4e01ba165302bf38947d2842620
Rina-an <arinandr@yahoo.com>
2023-12-04 12:18:47 +0300

    updated vendor info

--- analog_io_modules/seven_sensor_rain_gauge/firmware.lua
+++ analog_io_modules/seven_sensor_rain_gauge/firmware.lua
@@ -1,6 +1,6 @@
 local config = require('enapter.ucm.config')
 
-VENDOR = 'seven_sensor_solutions'
+VENDOR = 'Seven Sensor Solutions'
 
 -- Configuration variables must be also defined
 -- in `write_configuration` command arguments in manifest.yml
