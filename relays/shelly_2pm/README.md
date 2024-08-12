fab0408aa96501c6250c720c80d0805e3f4940e8
Nikolay V. Krasko <nikolay@enapter.com>
2024-08-12 17:51:40 +0400

    adding initial integration for Shelly Plus 2PM

---
+++ relays/shelly_2pm/README.md
@@ -0,0 +1,61 @@
+# Shelly Plus 2PM
+
+This [Enapter Device Blueprint](https://go.enapter.com/marketplace-readme) integrates **Shelly Plus 2PMT** - power metering switch with 2x10A relays. It can be used to control external electrical load such as lights, pumps and valves. It also can be used as roller shutter. This blueprint runs on Virtual UCM and integrates using [RPC over HTTP API](), therefore gateway and Shelly Plus 2PM module needs to be in one network. It also supports digest authentication.
+
+## Configuring Shelly Plus 2 PM
+
+1. Power On Module and wait when ShellyPlus2PM-*ID* Wi-Fi network will be discoverable in the air.
+2. Conntect to your ShellyPlus2PM-*ID* Wi-Fi network using your computer.
+
+    <img src="./.assets/01_Connect_To_Shelly.png" alt="Connect to Shelly Plus 2PM" width="25%" />
+
+3. Navigate to `http://192.168.33.1` using your favourite browser.
+
+    <img src="./.assets/02_Shelly_Web_UI.png" alt="Access Shelly Plus 2PM Web GUI" width="25%" />
+
+3. Navigate to `Settings`->`Wi-Fi` and set configuration of the network connection there. Select your Wi-Fi network from the list, put network password. It is also recommended to use static IPv4 settings.
+
+    <img src="./.assets/03_Configure_Wi-Fi.png" alt="Configure Shelly Plus 2PM Wi-Fi Network Settings" width="25%" />
+
+4. Ensure that Wi-Fi icon changed to active status.
+
+    <img src="./.assets/04_Wi-Fi_Connected.png" alt="Ensure that Wi-Fi is connected" width="25%" />
+
+5. It is also recommended to diable Access Point by removing `Enable AP Network` checkbox in the `Settings`->`Access Point` menu.
+
+    <img src="./.assets/05_Settings_Access_Point.png" alt="Disable Access Point" width="25%" />
+
+## Connect to Enapter
+
+1. Sign up to the Enapter Cloud using the [Web](https://cloud.enapter.com/) or mobile app ([iOS](https://apps.apple.com/app/id1388329910), [Android](https://play.google.com/store/apps/details?id=com.enapter&hl=en)).
+
+2. Use the [Enapter Gateway](https://handbook.enapter.com/software/gateway/2.0.0/setup/) to run the Virtual UCM.
+
+3. Create the [Enapter Virtual UCM](https://handbook.enapter.com/software/software.html#%F0%9F%92%8E-virtual-ucm).
+
+4. Upload thie Blueprint using [Enapter Marketplace](https://marketplace.enapter.com) on your mobile device. Advanced users can upload using Web IDE or CLI by following [Developer Documentation](https://developers.enapter.com/docs/tutorial/uploading-blueprint/).
+
+5. As soon as Blueprint will start the `IP Address Not Configured` event will be triggered.
+
+6. Click `Commands` on device screen.
+
+    <img src="./.assets/06_Commands.jpg" alt="Shelly Plus 2PM Blueprint Commands" width="25%" />
+
+7. On the commands list screen click on `Main Configuration` command:
+
+    <img src="./.assets/07_Configuration.jpg" alt="Shelly Plus 2PM Blueprint Configuration Settings" width="25%" />
+
+    You need to set the following parameters:
+
+    - IP Address
+
+    - Password (Optional)
+
+9. Press `Run` button
+
+The status data should be available on your dashboard as well as you will be able to use the valuies in [Enapter Rules Engine](https://developers.enapter.com/docs/reference/rules/time)
+
+## References
+
+- [Shelly Plus 2PM](https://www.shelly.com/en/products/shop/shelly-plus-2-pm)
+- [Shelly Plus 2PM RPC over HTTP API](https://shelly-api-docs.shelly.cloud/gen2/0.14/Devices/ShellyPlus2PM/)
---
+++ relays/shelly_2pm/src/main.lua
@@ -0,0 +1,306 @@
+local config = require('enapter.ucm.config')
+local json = require("json")
+local sha2 = require("sha2")
+
+-- Configuration variables must be also defined
+-- in `write_configuration` command arguments in manifest.yml
+IP_ADDRESS_CONFIG = 'address'
+PASSWORD="password"
+
+local CONNECTION = {}
+local TTY
+
+function get_auth(response, password)
+  if response.headers['Www-Authenticate'] and password then
+    math.randomseed(os.time())
+    math.random(); math.random(); math.random()
+    local cnonce = math.random(99999999)
+    local www_values = response.headers['Www-Authenticate'][1]
+    local realm = www_values:match('realm="([^"]+)"')
+    local nonce = www_values:match('nonce="([^"]+)"')
+    local ha1=sha2.sha256('admin:'..realm..':'..password)
+    local ha2=sha2.sha256('dummy_method:dummy_uri')
+    local result = sha2.sha256(tostring(ha1)..':'..tostring(nonce)..':1:'..tostring(cnonce)..':auth:'..tostring(ha2))
+    local auth = '"auth": { "realm":"'..realm..'", "username":"admin", "nonce":"'..nonce..'", "nc":"1", "qop":"auth", "cnonce":"'..cnonce..'", "response":"'..result..'", "algorithm":"SHA-256"}'
+    return auth, nil
+  else
+    return nil, 'No data available to make auth data'
+  end
+end
+
+function get_device_info()
+  -- Password is not needed to access this endpoint
+  local values, err = config.read_all()
+  if err then
+    enapter.log('cannot read config: '..tostring(err), 'error')
+    return nil, 'cannot_read_config'
+  else
+    local ip_address = values[IP_ADDRESS_CONFIG]
+
+    if not ip_address then
+      return nil, 'not_configured'
+    end
+
+    local response, err = http.get('http://'..ip_address..'/rpc/Shelly.GetDeviceInfo')
+
+    if err then
+      enapter.log('Cannot do request: '..err, 'error')
+      return nil, 'no_connection'
+    elseif response.code ~= 200 then
+      enapter.log('Request returned non-OK code: '..response.code, 'error')
+      return nil, 'wrong_request'
+    else
+      local jb = json.decode(response.body)
+      return jb, nil
+    end
+  end
+end
+
+function get_device_status()
+  local values, err = config.read_all()
+  if err then
+    enapter.log('cannot read config: '..tostring(err), 'error')
+    return nil, 'cannot_read_config'
+  else
+    local ip_address = values[IP_ADDRESS_CONFIG]
+    local password = values[PASSWORD]
+
+    if not ip_address then
+      return nil, 'not_configured'
+    end
+
+    local request, err = http.request('POST', 'http://'..ip_address..'/rpc','{"id":1, "method":"Shelly.GetStatus"}')
+    local client = http.client({ timeout = 5 })
+    local response, err = client:do_request(request)
+
+    if err then
+      enapter.log('Cannot do request: '..err, 'error')
+      return nil, 'no_connection'
+    elseif response.code == 401 then
+
+      if not password then
+        return nil, 'not_configured'
+      end
+
+      local auth = get_auth(response, password)
+      if auth then
+        local request, err = http.request('POST', 'http://'..ip_address..'/rpc', '{"id":1, "method":"Shelly.GetStatus",'..auth..'}')
+        local client = http.client({ timeout = 5 })
+        local response, err = client:do_request(request)
+
+        if response.code == 401 then
+          enapter.log('Request returned non-OK code: '..response.code, 'error')
+          return nil, 'wrong_password'
+        elseif response.code ~= 200 then
+          enapter.log('Request returned non-OK code: '..response.code, 'error')
+          return nil, 'wrong_request'
+        else
+          local jb = json.decode(response.body)
+          return jb, nil
+        end
+      else
+        return nil, 'wrong_request'
+      end
+    elseif response.code ~= 200 then
+      enapter.log('Request returned non-OK code: '..response.code, 'error')
+      return nil, 'wrong_request'
+    else
+      local jb = json.decode(response.body)
+      return jb, nil
+    end
+  end
+end
+
+function switch_on(switch, ctx)
+  local values, err = config.read_all()
+  if err then
+    enapter.log('cannot read config: '..tostring(err), 'error')
+    ctx.error(tostring(err))
+    return false, 'cannot_read_config'
+  else
+    local ip_address = values[IP_ADDRESS_CONFIG]
+    local password = values[PASSWORD]
+
+    if not ip_address then
+      ctx.error(tostring('not_configured'))
+      return false, 'not_configured'
+    end
+
+    local request, err = http.request('POST', 'http://'..ip_address..'/rpc','{"id":1, "method":"Switch.Set", "params":{"id":'..switch..',"on":true}}')
+    local client = http.client({ timeout = 5 })
+    local response, err = client:do_request(request)
+
+    if err then
+      enapter.log('Cannot do request: '..err, 'error')
+      ctx.error(tostring(err))
+      return false, 'no_connection'
+    elseif response.code == 401 then
+
+      if not password then
+        return false, 'not_configured'
+      end
+
+      local auth = get_auth(response, password)
+      if auth then
+        local request, err = http.request('POST', 'http://'..ip_address..'/rpc','{"id":1, "method":"Switch.Set", "params":{"id":'..switch..',"on":true}, '..auth..'}')
+        local client = http.client({ timeout = 5 })
+        local response, err = client:do_request(request)
+
+        if response.code == 401 then
+          ctx.error('Request returned non-OK code: '..response.code, 'error')
+          return false, 'wrong_password'
+        elseif response.code ~= 200 then
+          ctx.error('Request returned non-OK code: '..response.code, 'error')
+          return false, 'wrong_request'
+        else
+          return true, nil
+        end
+      else
+        return false, 'wrong_request'
+      end
+    elseif response.code ~= 200 then
+      ctx.error('Request returned non-OK code: '..response.code, 'error')
+      return false, 'wrong_request'
+    else
+      return true, nil
+    end
+  end
+end
+
+function switch_off(switch, ctx)
+  local values, err = config.read_all()
+  if err then
+    enapter.log('cannot read config: '..tostring(err), 'error')
+    ctx.error(tostring(err))
+    return false, 'cannot_read_config'
+  else
+    local ip_address = values[IP_ADDRESS_CONFIG]
+    local password = values[PASSWORD]
+
+    if not ip_address then
+      ctx.error(tostring('not_configured'))
+      return false, 'not_configured'
+    end
+
+    local request, err = http.request('POST', 'http://'..ip_address..'/rpc','{"id":1, "method":"Switch.Set", "params":{"id":'..switch..',"on":false}')
+    local client = http.client({ timeout = 5 })
+    local response, err = client:do_request(request)
+
+    if err then
+      enapter.log('Cannot do request: '..err, 'error')
+      ctx.error(tostring(err))
+      return false, 'no_connection'
+    elseif response.code == 401 then
+
+      if not password then
+        return false, 'not_configured'
+      end
+
+      local auth = get_auth(response, password)
+      if auth then
+        local request, err = http.request('POST', 'http://'..ip_address..'/rpc','{"id":1, "method":"Switch.Set", "params":{"id":'..switch..',"on":false}, '..auth..'}')
+        local client = http.client({ timeout = 5 })
+        local response, err = client:do_request(request)
+
+        if response.code == 401 then
+          ctx.error('Request returned non-OK code: '..response.code, 'error')
+          return false, 'wrong_password'
+        elseif response.code ~= 200 then
+          ctx.error('Request returned non-OK code: '..response.code, 'error')
+          return false, 'wrong_request'
+        else
+          return true, nil
+        end
+      else
+        return false, 'wrong_request'
+      end
+    elseif response.code ~= 200 then
+      ctx.error('Request returned non-OK code: '..response.code, 'error')
+      return false, 'wrong_request'
+    else
+      return true, nil
+    end
+  end
+end
+
+function main()
+  config.init({
+    [IP_ADDRESS_CONFIG] = { type = 'string', required = true },
+    [PASSWORD] = { type = 'string', defult=''},
+  })
+  scheduler.add(1000, send_properties)
+  scheduler.add(1000, send_telemetry)
+  -- Register command handlers
+  enapter.register_command_handler('switch_on_0', function(ctx) switch_on(0, ctx) end)
+  enapter.register_command_handler('switch_off_0',function(ctx) switch_off(0, ctx) end)
+  enapter.register_command_handler('switch_on_1', function(ctx) switch_on(1, ctx) end)
+  enapter.register_command_handler('switch_off_1', function(ctx) switch_off(1, ctx) end)
+end
+
+function send_properties()
+  local data, err = get_device_info()
+
+  if not err then
+    enapter.send_properties({
+      vendor = 'Shelly',
+      model = data['model'],
+      mac = data['mac'],
+      ver = data['ver'],
+    })
+  end
+end
+
+active_alerts = {}
+
+function send_telemetry()
+  local telemetry = {}
+  local status = 'Offline'
+
+  local data, err = get_device_status()
+
+  if err then
+    active_alerts = { err }
+  else
+    active_alerts = {}
+    status = 'Online'
+    telemetry['rssi'] = data['result']['wifi']['rssi']
+
+    for i = 0, 1, 1 do
+      if data['result']['switch:'..i]['output'] then
+        telemetry['switch'..i] = 'On'
+      else
+        telemetry['switch'..i] = 'Off'
+      end
+
+      telemetry['voltage'..i] = data['result']['switch:'..i]['voltage']
+      telemetry['current'..i] = data['result']['switch:'..i]['current']
+      telemetry['power'..i] = data['result']['switch:'..i]['apower']
+
+      if data['result']['input:'..i]['state'] then
+        telemetry['input'..i] = 'On'
+      else
+        telemetry['input'..i] = 'Off'
+      end
+    end
+  end
+
+  telemetry['alerts'] = active_alerts
+  telemetry['status'] = status
+
+  enapter.send_telemetry(telemetry)
+end
+
+function dump(o)
+  if type(o) == 'table' then
+     local s = '{ '
+     for k,v in pairs(o) do
+        if type(k) ~= 'number' then k = '"'..k..'"' end
+        s = s .. '['..k..'] = ' .. dump(v) .. ','
+     end
+     return s .. '} '
+  else
+     return tostring(o)
+  end
+end
+
+main()
